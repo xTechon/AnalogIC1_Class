766b44da41d8cd4ea68c6f57b60e2f24215225e9
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2025-10-20 22:38:02 -0400

    Add previous assignments

---
+++ flake.nix
@@ -0,0 +1,79 @@
+# from: https://flyx.org/nix-flakes-latex/
+{
+  description = "LaTeX Document Demo";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+  };
+  outputs = { self, nixpkgs, flake-utils, nix-vscode-extensions }:
+    with flake-utils.lib; eachSystem allSystems (system:
+      let
+        pkgs = import nixpkgs {inherit system; config.allowUnfree = true; };
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-medium latex-bin latexmk texcount setspace url 
+          algorithm2e relsize ifoddpage algpseudocodex algorithmicx fifo-stack varwidth 
+          tabto-ltx tabto-generic totcount tikzmark footmisc;
+        };
+        extensions = nix-vscode-extensions.extensions.${system};
+
+        codium = pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
+          vscodeExtensions = [
+            extensions.open-vsx.bbenoist.nix
+            extensions.open-vsx.jnoortheen.nix-ide
+            extensions.open-vsx.asvetliakov.vscode-neovim
+            extensions.open-vsx.zokugun.explicit-folding
+
+            extensions.open-vsx.efoerster.texlab
+            extensions.open-vsx.tecosaur.latex-utilities
+            extensions.open-vsx.james-yu.latex-workshop
+            pkgs.vscode-extensions.valentjn.vscode-ltex
+          ];
+        };
+
+        HW1Shell = pkgs.mkShell.override {} {
+          packages = [
+            codium
+            pkgs.texlab # lsp
+            pkgs.ltex-ls
+            tex # needed for development compilation
+            pkgs.perl # needed for latex-workshop
+          ];
+          shellHook = ''
+            export LABEL="autobiography"
+            alias code="codium autobiography.code-workspace"
+          '';
+        };
+
+      in
+      rec {
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "latex-demo-document";
+            src = self;
+            buildInputs = [ pkgs.coreutils tex ];
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              mkdir -p .cache/texmf-var
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf -lualatex \
+                HW1/HW1.tex \
+                macros.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp document.pdf $out/
+            '';
+          };
+        };
+        defaultPackage = packages.document;
+
+        devShells = {
+          default = HW1Shell;
+          HW1 = HW1Shell;
+        };
+
+      });
+}
